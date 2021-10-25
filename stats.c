94c7b10b5ed8fbf92feb5d34d113cf38457d505b
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2021-10-25 21:38:08 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,27 +1,31 @@
 #include "stats.h"
 #include "email_alerter.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
+
+struct Stats compute_statistics(const float* numberset, int setlength) 
+{
     struct Stats s;
     char size =0;
     int array_size;
     float max_val,min_val,avg_val =0;
-     min_val =numberset[size];
+    min_val =numberset[size];
     max_val =numberset[size];
-      array_size =setlength;
+    array_size =setlength;
 
     while(setlength--)
     {    
         if(size<=2)
         {
-        if(numberset[size+1] <min_val )
-        {
-           min_val =numberset[size+1];
-        }
-         if(numberset[size+1] >max_val)
-        {
-            max_val =numberset[size+1];
-        }
+            if(numberset[size+1] <min_val )
+            {
+                min_val =numberset[size+1];
+            }
+            if(numberset[size+1] >max_val)
+            {
+                max_val =numberset[size+1];
+            }
         }
         avg_val +=numberset[size];
         size++;
@@ -34,8 +38,6 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     return s;
 }
 
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
 
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
