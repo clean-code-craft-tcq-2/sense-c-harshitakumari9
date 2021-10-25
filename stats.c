9b3d149f8aad916fdd7e5d4a653ec8c7e5317bbb
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2021-10-25 22:12:11 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -41,6 +41,8 @@ struct Stats compute_statistics(const float* numberset, int setlength)
 
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
+    maxThreshold = 0;
+    computedStats = 0;
     (*alerters[0])();
     (*alerters[1])();
     
